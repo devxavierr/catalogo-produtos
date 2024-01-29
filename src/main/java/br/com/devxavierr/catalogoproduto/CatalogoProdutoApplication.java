4b55a0476a04ef7ed1aeb5eb23f754146076fb8a
root <root@natti-001>
2024-01-29 15:40:56 -0300

    init + configuracao mongo

---
+++ src/main/java/br/com/devxavierr/catalogoproduto/CatalogoProdutoApplication.java
@@ -0,0 +1,15 @@
+package br.com.devxavierr.catalogoproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@EnableMongoRepositories
+@SpringBootApplication
+public class CatalogoProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdutoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/devxavierr/catalogoproduto/domain/category/Category.java
@@ -0,0 +1,4 @@
+package br.com.devxavierr.catalogoproduto.domain.category;
+
+public class Category {
+}
